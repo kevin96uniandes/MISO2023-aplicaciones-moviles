a0e5b864490ea4519bc8a6351428b249a6b1c858
LuisaTorresMon <lj.torresm1@uniandes.edu.co>
2023-11-24 17:03:21 -0500

    Pruebas Espresso

--- app/src/main/java/com/uniandes/vinyls/network/Mocks/MockPerformerServiceAdapter.kt
+++ app/src/main/java/com/uniandes/vinyls/network/Mocks/MockPerformerServiceAdapter.kt
@@ -17,16 +17,22 @@ class MockPerformerServiceAdapter (context: Context) : NetworkServiceAdapter(con
     }
 
     fun findAll() : List<Performer> {
-        return listOf()
+        return listOf(Performer(1,"Taylor Alison Swift","https://es.wikipedia.org/wiki/Archivo:Taylor_Swift_at_the_2023_MTV_Video_Music_Awards_(1).png",
+                "es una cantautora, productora, directora, actriz y empresaria estadounidense.","1989-12-13T00:00:00.000Z"),
+            Performer(2,"Aubrey Drake Graham","https://es.wikipedia.org/wiki/Drake_(m%C3%BAsico)#/media/Archivo:Drake_en_2021_con_su_peinado_de_la_epoca.jpg",
+                "conocido simplemente como Drake, es un rapero, cantante, compositor, productor discográfico y actor canadiense.","1986-10-24T00:00:00.000Z"),
+            Performer(3,"Abel Makkonen Tesfaye","https://es.wikipedia.org/wiki/The_Weeknd#/media/Archivo:The_Weeknd_Portrait_by_Brian_Ziff.jpg",
+                "conocido artísticamente como The Weeknd, es un cantante, rapero, compositor y productor discográfico canadiense.","1990-02-16T00:00:00.000Z")
+        )
     }
 
     suspend fun getPerformerById(id: Int): Performer {
 
         var performer: Performer = Performer(1,
-            "Adele",
-            "https://albumart.publicradio.org/mb/78/7822977a-984d-358d-823f-426139c0eaba_a4bc.jpg",
-            "Adele Laurie Blue Adkins, conocida simplemente como Adele, es una cantautora y multinstrumentista británica.",
-            "2023-03-11T00:00:00.000Z"
+            "Taylor Alison Swift",
+            "https://es.wikipedia.org/wiki/Archivo:Taylor_Swift_at_the_2023_MTV_Video_Music_Awards_(1).png",
+            "es una cantautora, productora, directora, actriz y empresaria estadounidense.",
+            "1989-12-13T00:00:00.000Z"
         )
 
         return performer
